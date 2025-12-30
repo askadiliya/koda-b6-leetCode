67e429497c1f32b6c8f572d8ccc368dac7a127ca
Aska Diliyan Perkasa <93531147+askadiliya@users.noreply.github.com>
2025-12-30 17:32:59 +0700

    Add files via upload

---
+++ taks6.js
@@ -0,0 +1,14 @@
+// 2703 Retrun length of Aruguments Passed
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
